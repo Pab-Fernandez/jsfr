1a71b8cdef2e9c8c733acad723f61976ed534047
Pab-Fernandez <pablower@gmail.com>
2023-04-21 07:06:30 -0300

    algunas cositas sobre hooks

--- iteradoresagain.js
+++ iteradoresagain.js
@@ -17,10 +17,17 @@ const arrayMap = amiigos.map (function (pet){
 })
 
 
-// TODO Estos son states-
+// TODO             Estos son states-
 //const [cliente, setCliente] = useState({});//aqui objeto
 //const [total, setTotal] =useState (0);//aquí numeroto
 //const [clientes, setClientes] = useState ([]); //aqui es un array
 //const [modal, setModal] = useState (false);
 // React Renderiza cada vez que el state cambia. Para cambiarlo
-//? declaramos el state en nuestro componente. Es decor [variable, SetVariable]
+//? declaramos el state en nuestro componente. Es decor [variable, setVariable]
+//? El segundo valor, función modificadora, es la que la declara. Aquí "setVariable"
+//TODO                  Hooks
+// Se colocan en laparte superior de los componentes en React.
+//No puden ir en condicionales, y van antes del return. 
+// ? const [cliente, setCliente] = useState({})
+//?     abajo de esto va. no pueden in ir if ni el return. 
+
