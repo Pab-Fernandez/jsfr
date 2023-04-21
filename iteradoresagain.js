4528d3dda788b29b2bff7ee0b026bda3c9b5d07d
Pab-Fernandez <pablower@gmail.com>
2023-04-21 06:28:38 -0300

    algo de react

--- iteradoresagain.js
+++ iteradoresagain.js
@@ -4,12 +4,23 @@ const amiigos = ['Pomo', 'Piku', 'Kumo', 'Corgnelius']
 
 //forEach
 
-amiigos.forEach (function (pet){
+const arrayForeach = amiigos.forEach (function (pet){
     console.log (pet)
 })
 
 //map
+
 console.warn ('Aquí usando map')
-amiigos.map (function (pet){
+//Permite crear un nuevo arreglo
+const arrayMap = amiigos.map (function (pet){
     console.log (pet)
 })
+
+
+// TODO Estos son states-
+//const [cliente, setCliente] = useState({});//aqui objeto
+//const [total, setTotal] =useState (0);//aquí numeroto
+//const [clientes, setClientes] = useState ([]); //aqui es un array
+//const [modal, setModal] = useState (false);
+// React Renderiza cada vez que el state cambia. Para cambiarlo
+//? declaramos el state en nuestro componente. Es decor [variable, SetVariable]
